5481350459c00ac49b7489c96f8596129eb76e24
Denn Zhokhoff <d3z41k@gmail.com>
2018-01-23 18:01:57 +0200

    Add place keyboard + place

--- index.js
+++ index.js
@@ -23,12 +23,30 @@ const KB = {
     back: 'Назад'
   },
   place: {
-    queensland: 'Queensland',
-    southAustralia: 'South Australia',
-    tasmania: 'Tasmania',
-    newSouthWealth: 'New South Wealth',
-    westernAustralia: 'Western Australia',
-    northernTerritory: 'Northern Territory'
+    queensland: {
+      name: 'Queensland',
+      coordinates: '54.6994947260683,20.5036172478323',
+    },
+    southAustralia: {
+      name: 'South Australia',
+      coordinates: '57.6994947260683,27.5036172478323',
+    },
+    tasmania: {
+      name: 'Tasmania',
+      coordinates: '56.6994947260683,28.5036172478323',
+    },
+    newSouthWealth: {
+      name: 'New South Wealth',
+      coordinates: '78.6994947260683,35.5036172478323',
+    },
+    westernAustralia: {
+      name: 'Western Australia',
+      coordinates: '58.6994947260683,27.5036172478323',
+    },
+    northernTerritory: {
+      name: 'Northern Territory',
+      coordinates: '74.6994947260683,79.5036172478323',
+    }
   },
   transport: {
     auto: 'Прокат Авто',
@@ -51,6 +69,24 @@ bot.on('message', ctx => {
   switch(ctx.message.text) {
     case KB.main.attractions:
       sendAttractionsScreen(ctx)
+      break
+    case KB.place.queensland.name:
+      sendMap(ctx, KB.place.queensland.coordinates)
+      break
+    case KB.place.southAustralia.name:
+      sendMap(ctx, KB.place.southAustralia.coordinates)
+      break
+    case KB.place.tasmania.name:
+      sendMap(ctx, KB.place.tasmania.coordinates)
+      break
+    case KB.place.newSouthWealth.name:
+      sendMap(ctx, KB.place.newSouthWealth.coordinates)
+      break
+    case KB.place.westernAustralia.name:
+      sendMap(ctx, KB.place.westernAustralia.coordinates)
+      break
+    case KB.place.northernTerritory.name:
+      sendMap(ctx, KB.place.northernTerritory.coordinates)
       break
     case KB.main.back:
       sendGreeting(ctx, false)
@@ -81,17 +117,17 @@ function sendAttractionsScreen(ctx) {
 
     return ctx.reply(text,
         Markup.keyboard([
-         [KB.place.queensland, KB.place.southAustralia],
-         [KB.place.tasmania, KB.place.newSouthWealth],
-         [KB.place.westernAustralia, KB.place.northernTerritory],
+         [KB.place.queensland.name, KB.place.southAustralia.name],
+         [KB.place.tasmania.name, KB.place.newSouthWealth.name],
+         [KB.place.westernAustralia.name, KB.place.northernTerritory.name],
          [KB.main.back]
         ]).extra()
       )
 }
 
-function sendMap(ctx) {
+function sendMap(ctx, coordinates) {
 
-  const link = 'https://www.google.com/maps?q=54.6994947260683,20.5036172478323'
+  const link = `https://www.google.com/maps?q=${coordinates}`
   
   return ctx.reply(link).extra()
   /* type 2 */
